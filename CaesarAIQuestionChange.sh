2d840673608ea55c3c13c9001a5d19b1cbe99a6f
amari <amari.lawal@gmail.com>
2024-02-04 00:29:08 +0000

    add starting script

---
+++ CaesarAIQuestionChange.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /home/amari/CaesarAIQuestionChange
+
+/home/amari/miniconda3/envs/caesaraicontractqa/bin/python /home/amari/CaesarAIQuestionChange/main.py
+chromium http://127.0.0.1:9000
